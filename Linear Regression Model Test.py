e64e71ef7b9eda7f092ace3bad8b5bfb174a8eac
Sopheara Hean <heasy014@mymail.unisa.edu.au>
2025-01-04 15:32:15 +1030

    Fixed spelling error

--- Linear Regression Model Test.py
+++ Linear Regression Model Test.py
@@ -2,9 +2,9 @@ import numpy as np
 
 #Creating a fake dataset to use against a linear regression model
 xValues = [i for i in range(11)]
-xTrain = np.arrary(xValues, dtype=np.float32)
+xTrain = np.array(xValues, dtype=np.float32)
 xTrain = xTrain.reshape(-1, 1)
 
 yValues = [2*i + 1 for i in xValues]
-yTrain = np.arrary(yValues, dtype=np.float32)
+yTrain = np.array(yValues, dtype=np.float32)
 yTrain = yTrain.reshapre(-1, 1)
