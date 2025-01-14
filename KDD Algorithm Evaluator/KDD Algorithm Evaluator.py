0fdcc3c00378caba50c30e7d72f4d364c7fe340e
S454-cyber <heasy014@mymail.unisa.edu.au>
2025-01-14 18:09:04 +1030

    Added names for each column of the dataset to identify what values relate to what category

--- KDD Algorithm Evaluator/KDD Algorithm Evaluator.py
+++ KDD Algorithm Evaluator/KDD Algorithm Evaluator.py
@@ -11,7 +11,6 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 import warnings
-
 warnings.filterwarnings ('ignore')
 #%matplotlib warnings: A magical function that is only valid in Jupyter notebooks. Commented out as it is not valid in python.
 pd.set_option ('display.float_format', lambda x: '%.3f' % x)
@@ -20,11 +19,65 @@ plt.rcParams ["figure.figsize"] = (10,6)
 #READING DATASET
 #Reading the CSV file named "KDDTrain+.txt" stored locally in the same location as this python file using pandas.
 #Variable name df_0 has been given.
-df_0 = pd.read_csv ("KDDTrain+.txt") 
+readTrainFile = pd.read_csv ("KDDTrain+.txt") 
 #Variable df_0 is using the .copy() method to create copies of the "KDDTrain+.txt" file.
 #New variable name of df has been given.
-df = df_0.copy()
+organisedFile = readTrainFile.copy()
+#ADJUSTING COLUMNS
+columns = (['duration'
+            ,'protocolType'
+            ,'service'
+            ,'flag'
+            ,'sourceByte'
+            ,'destinationByte'
+            ,'land'
+            ,'wrongFragemnt'
+            ,'urgent'
+            ,'hot'
+            ,'numberFailedLogins'
+            ,'loggedIn'
+            ,'numberCompromised'
+            ,'rootShell'
+            ,'suAttempted'
+            ,'numRoot'
+            ,'numberFileCreations'
+            ,'numberShells'
+            ,'numberAccessFiles'
+            ,'numberOutboundCommands'
+            ,'isHostLogin'
+            ,'isGuestLogin'
+            ,'count'
+            ,'srvCount'
+            ,'serrorRate'
+            ,'srvSerrorRate'
+            ,'rerrorRate'
+            ,'srvRerrorRate'
+            ,'sameSrvRate'
+            ,'diffSrvRate'
+            ,'srvDiffHostRate'
+            ,'destinationHostCount'
+            ,'destinationHostSrvCount'
+            ,'destinationHostSameSrvCount'
+            ,'destinationHostDiffSrvCount'
+            ,'destinationHostSameSourcePortRate'
+            ,'destinationHostSrvDifferentHostRate'
+            ,'destinationHostSerrorRate'
+            ,'destinationHostSrvSerrorRate'
+            ,'destinationHostRerrorRate'
+            ,'destinationHostSrvRerrorRate'
+            ,'attack'
+            ,'level'])
+organisedFile.columns = columns
 #Displaying a section of the organised CSV file as a table to the screen. 
 #Displays 5 rows by default as no number of rows to display has been specified.
-df.head()
+organisedFile.head()
+#INSIGHTS
+organisedFile.info()
+organisedFile.describe().T
+
+#DATA CLEANING
+#NULL VALUES
+#DUPLCIATES
+#OUTLIERS
+#ATTACK OR NON ATTACK CLASSIFICATION
 
