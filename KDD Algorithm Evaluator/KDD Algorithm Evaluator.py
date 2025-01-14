b59ddb36068f4672520a5bd216eafedabfb8f30a
S454-cyber <heasy014@mymail.unisa.edu.au>
2025-01-14 16:25:16 +1030

    Fixed spelling errors

--- KDD Algorithm Evaluator/KDD Algorithm Evaluator.py
+++ KDD Algorithm Evaluator/KDD Algorithm Evaluator.py
@@ -21,7 +21,7 @@ plt.rcParams ["figure.figsize"] = (10,6)
 #Reading the CSV file named "KDDTrain+.txt" stored locally in the same location as this python file using pandas.
 #Variable name df_0 has been given.
 df_0 = pd.read_csv ("KDDTrain+.txt") 
-#Variable df_0 is using the .copy() method to creat copies of the "KDDTrain+.txt" file.
+#Variable df_0 is using the .copy() method to create copies of the "KDDTrain+.txt" file.
 #New variable name of df has been given.
 df = df_0.copy()
 #Displaying a section of the organised CSV file as a table to the screen. 
